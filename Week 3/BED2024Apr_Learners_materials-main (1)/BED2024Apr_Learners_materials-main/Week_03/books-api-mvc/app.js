1f32a9a2d84b347bc8e97eb7bcb92f4c06fdfb84
Chong Yun Ze <S10257927@connect.np.edu.sg>
2024-05-02 15:40:29 +0800

    Week 3, 2nd commit

--- Week 3/BED2024Apr_Learners_materials-main (1)/BED2024Apr_Learners_materials-main/Week_03/books-api-mvc/app.js
+++ Week 3/BED2024Apr_Learners_materials-main (1)/BED2024Apr_Learners_materials-main/Week_03/books-api-mvc/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const booksController = require("./controllers/booksController"); // Import controllers
-
+const validateBook = require("./middlewares/validateBook");
 
 const app = express();
 
@@ -11,8 +11,10 @@ app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
 
 app.get("/books", booksController.getAllBooks);
 app.get("/books/:id", booksController.getBookById);
-app.post("/books", booksController.createBook);
-app.put("/books/:id", booksController.updateBook);
+app.post("/books", validateBook, booksController.createBook); // Add validateBook before createBook
+app.put("/books/:id", validateBook, booksController.updateBook); // Add validateBook before updateBook
+// app.post("/books", booksController.createBook);
+// app.put("/books/:id", booksController.updateBook);
 app.delete("/books/:id", booksController.deleteBook);
 
 
@@ -22,9 +24,8 @@ app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
 
-const validateBook = require("./middlewares/validateBook");
-app.post("/books", validateBook, booksController.createBook); // Add validateBook before createBook
-app.put("/books/:id", validateBook, booksController.updateBook); // Add validateBook before updateBook
+
+
 
 
 
